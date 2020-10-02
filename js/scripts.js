95d8717e0a82faf70618146cf7ac19697304f881
kiarie <denniskiarie426@gmail.com>
2020-10-02 18:02:13 +0300

    modify the scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+if (d=0) {
+    console.log("sunday");
+  } else if (d=1) {
+    console.log("monday");
+  } else if (d=2) {
+    console.log("tuesday");
+  } else if (d=3){
+    console.log("wednesday");
+  } else if (d=4){ 
+    console.log("thursday"); 
+  } else if (d=5) { 
+    console.log("friday");
+  } else {          
+    console.log("saturday");
+  }
