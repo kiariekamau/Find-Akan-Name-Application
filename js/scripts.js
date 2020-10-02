16d38ae3f6a5a540ccd67a0fb574e1d3e596a048
kiarie <denniskiarie426@gmail.com>
2020-10-02 18:09:22 +0300

    add to the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 var date = function(CC,YY,MM,DD) {
     return  ( ((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD)%7
  }
- var CC=parseInt(prompt("Enter the century you were born,if its 2000 input 20:"));
+  var CC=parseInt(prompt("Enter the century you were born,if its 2000 input 20:"));
+  var YY = parseInt(prompt("Enter the year you were born,if its 1989 input 89:"));
+  var MM=parseInt( prompt("Enter the month you were born:"));
+  var DD= parseInt(prompt("Enter the day you were born:"));
 if (d=0) {
     console.log("sunday");
   } else if (d=1) {
